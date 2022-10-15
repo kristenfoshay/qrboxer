914bfae8c9b138454a107775bcc0c7e1f6035567
kristenfoshay <kristenfoshay@gmail.com>
2022-10-15 12:09:12 -0400

    updating config-overrides.js, package-lock.json, package.json

--- config-overrides.js
+++ config-overrides.js
@@ -11,7 +11,8 @@ module.exports = function override(config) {
         "https": require.resolve("https-browserify"),
         "os": require.resolve("os-browserify"),
         "url": require.resolve("url"),
-        "path": require.resolve("path-browserify")
+        "path": require.resolve("path-browserify"),
+        "buffer": require.resolve("buffer")
     })
     config.resolve.fallback = fallback;
     config.plugins = (config.plugins || []).concat([

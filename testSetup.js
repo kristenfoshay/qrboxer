dfa1e5d04b1a5e702709dd8935b5e83d75244250
kristenfoshay <88470531+kristenfoshay@users.noreply.github.com>
2024-11-08 13:10:07 -0500

    Create testSetup.js

---
+++ testSetup.js
@@ -0,0 +1,33 @@
+// tests/testSetup.js
+
+const db = require("./db");
+
+async function commonBeforeAll() {
+  // Clean database
+  await db.query("DELETE FROM boxes");
+  
+  // Add test data if needed
+  await db.query(`
+    INSERT INTO boxes (room, move)
+    VALUES ('Test Room', 1)
+  `);
+}
+
+async function commonBeforeEach() {
+  await db.query("BEGIN");
+}
+
+async function commonAfterEach() {
+  await db.query("ROLLBACK");
+}
+
+async function commonAfterAll() {
+  await db.end();
+}
+
+module.exports = {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll
+};
